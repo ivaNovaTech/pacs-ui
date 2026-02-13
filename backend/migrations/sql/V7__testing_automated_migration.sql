e572de0a3630fa588712e7720c5b81ad55e6f56c
ivaNova <jamesthairu@gmail.com>
2026-02-13 04:21:18 -0500

    testing CI/CD - dry run

--- backend/migrations/sql/V7__testing_automated_migration.sql
+++ backend/migrations/sql/V7__testing_automated_migration.sql
@@ -8,6 +8,6 @@ create table public.test3
 
 );
 
-insert into public.test  VALUES (1, 'row 1');
-insert into public.test  values (2, 'row 2');
-insert into public.test  values (3, 'row 3');
+insert into public.test3  VALUES (1, 'row 1');
+insert into public.test3  values (2, 'row 2');
+insert into public.test3  values (3, 'row 3');
