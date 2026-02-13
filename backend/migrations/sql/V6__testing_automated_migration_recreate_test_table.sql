8ce979fd4e9b128dfd194e6fc27e8e7fd243f2bd
ivaNova <jamesthairu@gmail.com>
2026-02-13 03:00:39 -0500

    more tests for db and application CI/CD delopyment through GitHub

---
+++ backend/migrations/sql/V6__testing_automated_migration_recreate_test_table.sql
@@ -0,0 +1,10 @@
+create table public test
+(
+    ID BIGINT,
+    col1 text
+
+);
+
+insert into test values (1, 'row 1');
+insert into test values (2, 'row 2');
+insert into test values (3, 'row 3');
